e3d2fbdbdb62aaf50a0b82a83414a79db13ac44b
Kushal Chinchanikar <kushal.chinchanikar1@gmail.com>
2022-01-16 16:42:44 +0530

    app.py minor change

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 # Importing required libraries
 from flask import Flask, render_template, request
 import pandas as pd
+#
 import numpy as np
 import tensorflow as tf
 from keras.preprocessing.image import load_img
